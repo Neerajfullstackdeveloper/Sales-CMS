646598a43881c3e305193bb8749cf1970b60a04a
Neeraj Pandey <137383536+Neerajfullstackdeveloper@users.noreply.github.com>
2025-11-21 11:54:47 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,4 @@ import App from "./App.tsx";
 import "./index.css";
 import "./jsx-global.js";
 
-createRoot(document.getElementById("root")!).render(<App className="bg-[#1D3FAE]" />);
+createRoot(document.getElementById("root")!).render(<App />);
