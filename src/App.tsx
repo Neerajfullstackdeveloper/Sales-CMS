f0b46bb6e2ac8033407f257fb04d9e161f94daed
atulkumar021 <atulp3655@gmail.com>
2026-02-10 17:00:27 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import LoginSelection from "./pages/LoginSelection";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
@@ -24,7 +23,7 @@ const App = () => (
         }}
       >
         <Routes>
-          <Route path="/" element={<LoginSelection />} />
+          <Route path="/" element={<Auth />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/dashboard" element={<Dashboard />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
